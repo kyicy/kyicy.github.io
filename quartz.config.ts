59f2b075fdad5fef954ba685f76f884fb932feb4
kyicy <37315426+kyicy@users.noreply.github.com>
2026-05-15 19:03:43 +0800

    Quartz sync: May 15, 2026, 7:03 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,7 +69,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "relative" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],

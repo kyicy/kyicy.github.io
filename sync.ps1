5b233609cc9a7b56274b1278d0a8b76483eb7e5d
kyicy <37315426+kyicy@users.noreply.github.com>
2026-05-20 22:24:19 +0800

    Quartz sync: 五月 20, 2026, 10:24 下午

--- sync.ps1
+++ sync.ps1
@@ -1,5 +1,5 @@
 Write-Host "Syncing changes to remote..."
 git add .
-$commitDate = Get-Date -Format "MMMM dd, yyyy, h:mm tt" -CultureInfo "en-US"
+$commitDate = Get-Date -UFormat "%B %d, %Y, %I:%M %p"
 git commit -m "Quartz sync: $commitDate" 2>$null; if ($LASTEXITCODE -ne 0) { Write-Host "No changes to commit" }
 git push
